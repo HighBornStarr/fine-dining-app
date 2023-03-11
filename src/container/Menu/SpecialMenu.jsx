57fbdaf9c7edcd8530f6c581ed6e01dd7047b485
Alex Starr <116136090+HighBornStarr@users.noreply.github.com>
2023-03-10 19:28:16 -0600

    SM commit

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -5,8 +5,26 @@ import { images, data } from '../../constants'
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className='app__specialMenu-title'>
+      <SubHeading title="Menu That Fits Your palatte" />
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
+
+    <div className='app__specialMenu-menu'>
+    <div className='app__specialMenu-menu_wine flex__center'>
+     <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+    </div>
+    <div className='app__specialMenu_menu_items'>
+      {data.wines.map((wine,index) => (<p>{wine.title}</p>
+       ))}
+    </div>
+
+    <div>
+      
+    </div>
+
+    </div>
   </div>
 );
 
